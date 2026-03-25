a1d6495e8caf843a57c5d748870611f03a5f7238
Chinmay Shet <chishxd@tutamail.com>
2026-03-26 00:37:54 +0530

    add drag to draw feature

--- src/main.rs
+++ src/main.rs
@@ -47,10 +47,15 @@ impl eframe::App for PixelArtApp{
                         .fill(pixel_color)
                         .min_size(egui::Vec2 { x: 32.0, y: 32.0 });
 
-                        if ui.add(button).clicked(){
-                            self.pixels[y][x] = self.current_color;
-                        }
+                        let button_response = ui.add(button);
 
+                        let is_pointer_over_button = ui.rect_contains_pointer(button_response.rect);
+
+                        if button_response.clicked()
+                         || (is_pointer_over_button && ui.input(|i| i.pointer.primary_down()))
+                        {
+                                self.pixels[y][x] = self.current_color;
+                        }
                     }
 
                 });
